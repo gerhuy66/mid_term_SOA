7f933f3a1e852e8bf87bd483c57b422a2bb4237c
huy le <huy.cdl@cyberlogitec.com>
2021-03-14 21:52:15 +0700

    delete old

--- mid_term/app/database/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-from app import app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/midterm_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-mysql_db = SQLAlchemy(app)
-
--- mid_term/app/models/Tution.py
+++
@@ -1,9 +0,0 @@
-from app.database import mysql_db
-class Tution(mysql_db.Model):
-    __tablename__ = 'tutions'
-    id = mysql_db.Column(mysql_db.Integer, primary_key=True)
-    student_id = mysql_db.Column(mysql_db.String(20))
-    fee = mysql_db.Column(mysql_db.Integer)
-
-    def __repr__(self):
-        return '<Tution %r>' % self.id
