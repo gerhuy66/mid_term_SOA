cec4ea123fde6430a23c6806df892bc7618426cc
quangphuong <quangphuong412@gmail.com>
2021-03-16 03:35:24 +0700

    getBalance

--- mid_term_2/app/routes/getBalance.py
+++ mid_term_2/app/routes/getBalance.py
@@ -6,6 +6,6 @@ from flask import json, render_template, request, session, Response,jsonify,redi
 @app.route("/balance",methods=['POST'])
 def getAccountBalance():
     stId = request.args.get('stId')
-    get_stId = Bank_Account.query.filter_by(Bank_Account.student_id == stId).first()
+    get_bln = Bank_Account.query.filter_by(Bank_Account.student_id == stId).first()
     bln = get_bln.balance
     return make_response(jsonify({"balance": bln}))
