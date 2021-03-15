bd879b67b2fb0f782f1f7662ea1fbf578dcde362
quangphuong <quangphuong412@gmail.com>
2021-03-16 03:10:14 +0700

    ha

---
+++ mid_term_2/app/routes/getBalance.py
@@ -0,0 +1,11 @@
+from app.models import User,Student,Role, Bank_Account
+import flask
+from app import app
+from flask import json, render_template, request, session, Response,jsonify,redirect,url_for,flash,make_response
+
+@app.route("/balance",methods=['POST'])
+def getAccountBalance():
+    stId = request.args.get('stId')
+    get_stId = Bank_Account.query.filter_by(Bank_Account.student_id == stId).first()
+    bln = get_bln.balance
+    return make_response(jsonify({"balance": bln}))
--- mid_term_2/app/routes/views.py
+++ mid_term_2/app/routes/views.py
@@ -13,8 +13,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-    Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
@@ -92,8 +91,8 @@ def createStu():
     rs = stu_schema.dump(student.create())
     return make_response(jsonify({"student": rs}),201)
 
-@app.route("getRoute/<userId>",methods=['GET'])
-def getRole(userId):
-    user = User.User.query.filter_by(userName=userId)
-    return make_response(jsonify({"Roles": user.roles}),201)
+# @app.route("getRoute/<userId>",methods=['GET'])
+#def getRole(userId):
+#    user = User.User.query.filter_by(userName=userId)
+#    return make_response(jsonify({"Roles": user.roles}),201)
     
