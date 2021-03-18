8f77593a547046a2b0b91e2bea0acee3a2a679d6
thaitrann <tranthai6693@gmail.com>
2021-03-18 15:57:27 +0700

    update mail

---
+++ mid_term_2/app/services/mailService.py
@@ -0,0 +1,20 @@
+from app import app
+from flask_mail import Mail, Message
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'ibankingmtsoa@gmail.com'
+app.config['MAIL_PASSWORD'] = 'aA!123456'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['SECRET_KEY'] = 'xxxxxxxxx'
+
+mail = Mail(app)
+
+
+
+def sendEmail(subject, email, content):
+    message = Message(subject, sender = 'ibankingmtsoa@gmail.com', recipients=[email])
+    message.body = content
+    mail.send(message)
+    return "Gửi mail xong đĩ mẹ m"
